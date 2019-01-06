0575f1c148b1285458a1874e6070b7fce7e61b67
Guilherme Oliveira <guilherme.ol.an@gmail.com>
2019-01-06 01:04:28 -0300

    Iniciando projeto do curso Alura

---
+++ IMC/TelaResultadosViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TelaResultadosViewController.swift
+//  IMC
+//
+//  Created by guilherme oliveira andrade on 01/01/2019.
+//  Copyright © 2019 Guilherme.Ol. All rights reserved.
+//
+
+import UIKit
+
+class TelaResultadosViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
