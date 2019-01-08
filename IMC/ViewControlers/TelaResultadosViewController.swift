1f1e2be9bc7e83c68cda86eb21fd30cb8f1bb4e1
Guilherme Oliveira <guilherme.ol.an@gmail.com>
2019-01-08 15:27:04 -0300

    lutando contras os bugs round 1

--- IMC/ViewControlers/TelaResultadosViewController.swift
+++ IMC/ViewControlers/TelaResultadosViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 
 class TelaResultadosViewController: UIViewController {
 
-
+    //Variables
+    @IBOutlet weak var pesoLabel: UILabel!
+    @IBOutlet weak var alturaLabel: UILabel!
+    @IBOutlet weak var idadeLabel: UILabel!
+    @IBOutlet weak var resultIMCLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    var stringPassed = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        alturaLabel.text = stringPassed
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
--- IMC/ViewControlers/ViewController.swift
+++ IMC/ViewControlers/ViewController.swift
@@ -17,27 +17,45 @@ class ViewController: UIViewController
     @IBOutlet weak var textFieldAltura: UITextField!
     @IBOutlet weak var textFieldPeso: UITextField!
     @IBOutlet weak var textFieldIdade: UITextField!
+    @IBOutlet weak var alturaLabel: UILabel!
     
+    @IBAction func btmlimpar(_ sender: Any)
+    {
+        textFieldAltura.text = ""
+        textFieldPeso.text = ""
+        textFieldIdade.text = ""
+        textFieldNomeCompleto.text = ""
+    }
     @IBAction func add()
     {
-        if textFieldNomeCompleto == nil || textFieldPeso == nil
-            || textFieldIdade == nil || textFieldAltura == nil
+        
+        if textFieldNomeCompleto == nil || textFieldNomeCompleto.text == ""
         {
-                return
+            textFieldNomeCompleto.placeholder = "Por favor, digite seu nome completo."
+            return
+        }
+        if textFieldIdade == nil || textFieldIdade.text == ""
+        {
+            textFieldIdade.placeholder = "Digite sua altura."
+            return
+        }
+        if textFieldPeso == nil || textFieldPeso.text == ""
+        {
+           textFieldPeso.placeholder = "Digite seu peso."
+            return
         }
         
-   //     if let happiness = Int(happinessField.text!)
-        
-        
-        let nome = textFieldNomeCompleto.text
-        let altura = Int(textFieldAltura.text!)
-        let peso = Double(textFieldPeso.text!)
-        let idade = Int(textFieldIdade.text!)
-    
-        
-        let valores = imc(nomeCompleto: nome ?? "", idade: idade ?? 0, peso: peso ?? 0.0, altura: altura ?? 0)
-        
-        print(valores.nomeCompleto)
+        if textFieldNomeCompleto != nil && textFieldIdade != nil{
+            let nome = textFieldNomeCompleto.text
+            let altura = Int(textFieldAltura.text!)
+            let peso = Double(textFieldPeso.text!)
+            let idade = Int(textFieldIdade.text!)
+            
+            
+            let valores = imc(nomeCompleto: nome ?? "", idade: idade ?? 0, peso: peso ?? 0.0, altura: altura ?? 0)
+            
+            print(valores.nomeCompleto)
+        }
         
     }
 }
